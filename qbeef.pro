c3d87057ef1d1067ec1fcaf0b81c28be699130e8
ganelonhb <justyouraveragejoe@outlook.com>
2023-05-29 15:16:42 -0700

    started Latin translation

--- qbeef.pro
+++ qbeef.pro
@@ -1,6 +1,6 @@
 QT       += core gui
 
-greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets xml
 
 CONFIG += c++20
 
@@ -19,7 +19,9 @@ FORMS += \
     mainwindow.ui
 
 TRANSLATIONS += \
-    qbeef_en_US.ts
+    qbeef_en_US.ts \
+    qbeef_la.ts
+
 CONFIG += lrelease
 CONFIG += embed_translations
 
@@ -30,3 +32,6 @@ else: unix:!android: target.path = /opt/$${TARGET}/bin
 
 RESOURCES += \
     resources.qrc
+
+DISTFILES += \
+    qbeef_la.ts
---
+++ qbeef_la.ts
@@ -0,0 +1,275 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="la">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>QBEEF</source>
+        <translation>QBEEF</translation>
+    </message>
+    <message>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;A path to a file containing test input values (for interactive CLI programs)&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Open the file dialog for the &lt;span style=&quot; font-weight:700;&quot;&gt;Input File &lt;/span&gt;field&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Clear the path in the &lt;span style=&quot; font-weight:700;&quot;&gt;Input File &lt;/span&gt;field&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Additional Settings:</source>
+        <translation>Additicius Occasus:</translation>
+    </message>
+    <message>
+        <source>Input File (Optional):</source>
+        <translation>Initalis Arquivus (Ad Libitum):</translation>
+    </message>
+    <message>
+        <source>Program Executable:</source>
+        <translation>Programa Exsecus:</translation>
+    </message>
+    <message>
+        <source>Arguments:</source>
+        <translation>Programae Moduli:</translation>
+    </message>
+    <message>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Open the file dialog for the &lt;span style=&quot; font-weight:700;&quot;&gt;Program Executable &lt;/span&gt;field&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>General</source>
+        <translation>Generalis</translation>
+    </message>
+    <message>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;How deep will valgrind Memcheck consider backtraces when merging leaks into a report&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>High</source>
+        <translation>Superus</translation>
+    </message>
+    <message>
+        <source>Medium</source>
+        <translation>Mediocris</translation>
+    </message>
+    <message>
+        <source>Low</source>
+        <translation>Humilis</translation>
+    </message>
+    <message>
+        <source>Leak Resolution:</source>
+        <translation>Rima Resolucio:</translation>
+    </message>
+    <message>
+        <source>Leak Types:</source>
+        <translation>Generis Rimae:</translation>
+    </message>
+    <message>
+        <source>All</source>
+        <translation>Totus</translation>
+    </message>
+    <message>
+        <source>Indirect</source>
+        <translation>Obliquus</translation>
+    </message>
+    <message>
+        <source>Reachable</source>
+        <translation>Tangibilis</translation>
+    </message>
+    <message>
+        <source>Possible</source>
+        <translation>Veri Similis</translation>
+    </message>
+    <message>
+        <source>Definite</source>
+        <translation>Certus</translation>
+    </message>
+    <message>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;How detailed should the leak check be? Corresponds to --leak-check in valgrind&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Normal</source>
+        <translation>Solitus</translation>
+    </message>
+    <message>
+        <source>Summary</source>
+        <translation>Subitus</translation>
+    </message>
+    <message>
+        <source>Full</source>
+        <translation>Expletus</translation>
+    </message>
+    <message>
+        <source>Leak-Check:</source>
+        <translation>Rima Incipio:</translation>
+    </message>
+    <message>
+        <source>Heuristics:</source>
+        <translation>Heuristicae:</translation>
+    </message>
+    <message>
+        <source>stdstring</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>length64</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>newarray</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>multipleinheritance</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Advanced</source>
+        <translation>Provectus</translation>
+    </message>
+    <message>
+        <source>track-origins</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>undef-value-errors</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>free-fill:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>partial-loads-ok</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>malloc-fill:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>expensive-definedness-checks:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>yes</source>
+        <translation>ita</translation>
+    </message>
+    <message>
+        <source>auto</source>
+        <translation>automatus deprensio</translation>
+    </message>
+    <message>
+        <source>no</source>
+        <translation>minime</translation>
+    </message>
+    <message>
+        <source>ignore-range-below-sp:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>workaround-gcc296-bugs</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>to</source>
+        <translation>ad</translation>
+    </message>
+    <message>
+        <source>Volumes and Ranges:</source>
+        <translation>Volumina et Septa:</translation>
+    </message>
+    <message>
+        <source>freelist-vol:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Boolean CLI Options:</source>
+        <translation>IVI Arbitria Booli:</translation>
+    </message>
+    <message>
+        <source>show-mismatched-frees</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>freelist-big-blocks:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>keep-stacktraces-alloc</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>alloc</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>free</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>alloc-and-free</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>alloc-then-free</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>none</source>
+        <translation>nihil</translation>
+    </message>
+    <message>
+        <source>Multi-Valued CLI Options:</source>
+        <translation>IVI Arbitria Multiformis:</translation>
+    </message>
+    <message>
+        <source>Fill Options:</source>
+        <translation>Bita-Satiata Arbitria:</translation>
+    </message>
+    <message>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Clear the path in the &lt;span style=&quot; font-weight:700;&quot;&gt;Program Executable &lt;/span&gt;field&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Report:</source>
+        <translation>Litteræ:</translation>
+    </message>
+    <message>
+        <source>Generate Report</source>
+        <translation>Litteræ Fabrica</translation>
+    </message>
+    <message>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Save the report to disk&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Litteræ in discum reservavero&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <source>Save Report</source>
+        <translation>Litteræ Reserva</translation>
+    </message>
+    <message>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;A path to an executable on disk.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>File</source>
+        <translation>Arquivus</translation>
+    </message>
+    <message>
+        <source>Clear</source>
+        <translation>Dele</translation>
+    </message>
+    <message>
+        <source>Open Executable</source>
+        <translation>Exsecum Evolve</translation>
+    </message>
+    <message>
+        <source>Open Text File</source>
+        <translation>Arquivum Verborum Evolve</translation>
+    </message>
+</context>
+</TS>

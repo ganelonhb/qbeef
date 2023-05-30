bca490a81ee7eca51cf37a784515a22d9a5c0243
Zachary John Worcester <zworcester0@csu.fullerton.edu>
2023-05-30 15:27:32 -0700

    began implementing DOM tree interpreter for viewing valgrind output as tree

--- dommodel.h
+++ dommodel.h
@@ -1,14 +1,37 @@
 #ifndef DOMMODEL_H
 #define DOMMODEL_H
 
+// Uses implementation defined here:
+//  https://doc.qt.io/qt-5/qtwidgets-itemviews-simpledommodel-example.html
+
 #include <QAbstractItemModel>
 #include <QObject>
+#include <QDomDocument>
+#include <QVariant>
+#include <QModelIndex>
+
+#include "domitem.h"
 
 class DomModel : public QAbstractItemModel
 {
     Q_OBJECT
 public:
-    explicit DomModel(QObject *parent = nullptr);
+    explicit DomModel(const QDomDocument& document, QObject *parent = nullptr);
+    ~DomModel();
+
+    QVariant data(const QModelIndex& index, int role) const override;
+    Qt::ItemFlags flags(const QModelIndex& index) const override;
+    QVariant headerData(    int section, Qt::Orientation orientation,
+                            int role = Qt::DisplayRole) const override;
+    QModelIndex index(int row, int column,
+                      const QModelIndex& parent = QModelIndex()) const override;
+    QModelIndex parent(const QModelIndex& child) const override;
+    int rowCount(const QModelIndex& parent = QModelIndex()) const override;
+    int columnCount(const QModelIndex& parent = QModelIndex()) const override;
+
+private:
+    QDomDocument domDocument;
+    DomItem* rootItem;
 };
 
 #endif // DOMMODEL_H
